782baadd50ee7e27c9b613955d518f7b7f6e7861
adhoc <postma.marin@protonmail.com>
2024-06-25 18:12:26 +0200

    feature gate rusqlite

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
+#[cfg(feature = "allocator")]
 mod alloc;
+#[cfg(feature = "allocator")]
 mod local_tracker;
-// mod id;
 mod proto;
+
+#[cfg(feature = "parse")]
 mod parse;
 
+#[cfg(feature = "allocator")]
 pub use alloc::{Allocator, enable_tracking, disable_tracking, TrackerConfig};
 
 #[doc(hidden)]
+#[cfg(feature = "parse")]
 pub use parse::parse_profile;
